670bf0cf83d39ad01fb4bf04d56e9ebd83a8c278
tomgosling57 <tomgosling57@gmail.com>
2021-09-08 12:30:37 +1000

    basic house function (doesnt always work not sure if due to pycharm tho)

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create()
+player = mc.player
+
+
+def buildHouse(l,h,w):
+    px, py, pz = player.getPos()
+
+    px, py, pz = int(px), int(py), int(pz)
+    px+=2
+
+    # Walls
+    mc.setBlocks(px, py, pz, px, py+h, pz+w, 45)
+    mc.setBlocks(px, py, pz, px+l, py+h, pz, 45)
+    mc.setBlocks(px+l, py, pz, px+l, py+h, pz+w, 45)
+    mc.setBlocks(px, py, pz+w, px+l, py+h, pz+w, 45)
+
+    #roof and floor
+
+
+buildHouse(8,5,8)
+
+
+
+
