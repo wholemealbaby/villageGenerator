88787c0380b9129059d59fdea3f816ddcb236d5b
wilsonluc <wilsonluc09@gmail.com>
2021-09-08 15:42:03 +1000

    Updated my class

--- wilson.py
+++ wilson.py
@@ -1,27 +1,34 @@
 from mcpi.minecraft import Minecraft
-import numpy as np
-import random
+from House import House
 
 mc = Minecraft.create()
 x, y, z = mc.player.getPos()
 
 brick = 46
+air = 0
 oak = 5
+glass = 20
 
 
-def create_house(length, width, height):
+def create_house(length=8, width=10, height=5):
     # Makes a rectangle
     for i in range(length):
         for j in range(width):
             for k in range(height):
                 if k == 0 or k == height - 1:
-                    mc.setBlock(x + i, y + k, z + j, 5)
+                    mc.setBlock(x + i, y + k, z + j, oak)
                 elif i == 0 or i == length - 1:
-                    mc.setBlock(x + i, y + k, z + j, 5)
+                    mc.setBlock(x + i, y + k, z + j, oak)
                 elif j == 0 or j == width - 1:
-                    mc.setBlock(x + i, y + k, z + j, 5)
+                    mc.setBlock(x + i, y + k, z + j, oak)
                 else:
-                    mc.setBlock(x + i, y + k, z + j, 0)
+                    mc.setBlock(x + i, y + k, z + j, air)
 
 
-create_house(8, 10, 5)
+house_length = 20
+house_height = 5
+house_width = 20
+
+house1 = House(x, y, z, x + house_length, y + house_height, z + house_width, oak)
+
+# create_house()
