6158f64e865c4f5bb365b04efdd620ffb2a9cb39
wilsonluc <wilsonluc09@gmail.com>
2021-09-08 15:15:20 +1000

    Skeleton for classes

---
+++ House.py
@@ -0,0 +1,14 @@
+class House:
+    def __init__(self, x_lower, x_upper, y_lower, y_upper, z_lower, z_upper):
+        self.x_lower = x_lower
+        self.x_upper = x_upper
+
+        self.y_lower = y_lower
+        self.y_upper = y_upper
+
+        self.z_lower = z_lower
+        self.z_upper = z_upper
+
+        rooms = []
+
+
