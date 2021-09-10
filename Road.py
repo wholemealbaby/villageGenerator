be84e5530234d0290d742e282b6efc999e36ef02
wilsonluc <wilsonluc09@gmail.com>
2021-09-10 11:47:46 +1000

    Updated road algorithm to place under x, y, z

--- Road.py
+++ Road.py
@@ -2,39 +2,42 @@ from mcpi.minecraft import Minecraft
 import random
 
 
-def relative_movement(bearing, coords, direction, length):
-    x, y, z = int(coords[0]), int(coords[1]), int(coords[2])
-    if direction == 'up':
-        return x, y + length, z
-    elif direction == 'down':
-        return x, y - length, z
-    elif direction == 'left' or direction == 'right':
-        if direction == 'right':
-            length *= -1
-        if bearing == 'north':
-            return x - length, y, z
-        elif bearing == 'east':
-            return x, y, z - length
-        elif bearing == 'south':
-            return x + length, y, z
-        elif bearing == 'west':
-            return x, y, z + length
-    elif direction == 'forward' or direction == 'back':
-        if direction == 'back':
-            length *= -1
-        if bearing == 'north':
-            return x, y, z - length
-        elif bearing == 'east':
-            return x + length, y, z
-        elif bearing == 'south':
-            return x, y, z + length
-        elif bearing == 'west':
-            return x - length, y, z
+def relative_movement(bearing, coords, instructions):
+    x, y, z = coords[0], coords[1], coords[2]
+    for i in instructions:
+        direction = i[0]
+        length = i[1]
+        if direction == 'up':
+            y += length
+        elif direction == 'down':
+            y -= length
+        elif direction == 'left' or direction == 'right':
+            if direction == 'right':
+                length *= -1
+            if bearing == 'north':
+                x -= length
+            elif bearing == 'east':
+                z -= length
+            elif bearing == 'south':
+                x += length
+            elif bearing == 'west':
+                z += length
+        elif direction == 'forward' or direction == 'back':
+            if direction == 'back':
+                length *= -1
+            if bearing == 'north':
+                z -= length
+            elif bearing == 'east':
+                x += length
+            elif bearing == 'south':
+                z += length
+            elif bearing == 'west':
+                x -= length
+    return x, y, z
 
-
+# Spawn road under x, y, z
 class Road:
     def __init__(self, bearing, x, y, z, length, width, winding_dist, block):
-        x, y, z = int(x), int(y), int(z)
         self.bearing = bearing
         self.x, self.y, self.z = x, y, z
         self.length, self.width = length, width - 1
@@ -43,8 +46,9 @@ class Road:
 
         self.road_section = []
         # Init road_section with starting coords
-        self.road_section.append(
-            [(x, y, z), (relative_movement(bearing, (self.x, self.y, self.z), 'right', self.width))])
+        starting_coords = relative_movement(bearing, (self.x, self.y, self.z),
+                                            [('down', 1)])
+        self.road_section.append(starting_coords)
 
         self.mc = Minecraft.create()
 
@@ -57,17 +61,22 @@ class Road:
                 curve = random.choice([-1] * 1 + [0] * 8 + [1] * 1)
                 if -self.winding_dist <= cur_curve + curve <= self.winding_dist:
                     valid_bounds = True
-                    new_base_coord = relative_movement(self.bearing, self.road_section[-1][0], 'forward', 1)
                     if curve == -1:
-                        new_base_coord = relative_movement(self.bearing, new_base_coord, 'left', 1)
+                        road_left = relative_movement(self.bearing, self.road_section[-1], [('forward', 1), ('left', 1)])
+                        self.road_section.append(road_left)
+                    elif curve == 0:
+                        road_left = relative_movement(self.bearing, self.road_section[-1], [('forward', 1)])
+                        self.road_section.append(road_left)
                     elif curve == 1:
-                        new_base_coord = relative_movement(self.bearing, new_base_coord, 'right', 1)
-                    self.road_section.append(
-                        [new_base_coord, (relative_movement(bearing, new_base_coord, 'right', self.width))])
+                        road_left = relative_movement(self.bearing, self.road_section[-1], [('forward', 1), ('right', 1)])
+                        self.road_section.append(road_left)
 
         for coords in self.road_section:
-            print(coords[0], coords[1])
-            self.mc.setBlocks(coords[0], coords[1], block)
+            road_left = coords
+            road_right = relative_movement(self.bearing, road_left, [('right', self.width)])
+            self.mc.setBlocks(road_left, road_right, block)
 
     def get_coords(self):
+        for i in self.road_section:
+            print(i)
         return self.road_section
